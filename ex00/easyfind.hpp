8b87e92d52a7dd6bed21f04757ee1c2c59f014a3
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-24 11:29:15 +0100

    add new file .hppa and makefile

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/21 19:10:17 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/24 11:17:09 by ahajji           ###   ########.fr       */
+/*   Created: 2024/08/24 11:16:26 by ahajji            #+#    #+#             */
+/*   Updated: 2024/08/24 11:16:39 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
 
-int main()
+template<typename T>
+int    easyfind(T array, int find)
 {
-    std::vector<int> arr;
-    arr.push_back(1);
-    arr.push_back(9);
-    arr.push_back(5);
-    arr.push_back(2);
-    std::cout << "this is the index : " << easyfind(arr, 9) << std::endl;
-    return 0;
+    int i = 0;
+    while (i < array.size())
+    {
+        if(array[i] == find)
+            return i;
+        i++;
+    }
+    return -1;
 }
